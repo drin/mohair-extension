6995f925871dbd666415fcbc436462723b461661
Aldrin Montana <octalene.dev@pm.me>
2023-02-03 13:55:13 -0800

    Adding initial sources

---
+++ mohair_extension/test_dependencies.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# ------------------------------
+# Dependencies
+
+import ibis
+
+from mohair_extension.mohair import algebra_pb2 as mohair_algebra
+from ibis_substrait.compiler.core import SubstraitCompiler
